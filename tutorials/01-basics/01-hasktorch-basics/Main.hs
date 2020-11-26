7dc8b02d83e37a630bd5774db6558689fd193fad
Cmdv <vincent@cmdv.me>
2020-11-26 17:34:02 +0000

    finishing differentiation and continue with autoGradient

--- tutorials/01-basics/01-hasktorch-basics/Main.hs
+++ tutorials/01-basics/01-hasktorch-basics/Main.hs
@@ -8,6 +8,7 @@ import Torch.Random (mkGenerator, randn')
 import Torch.Device (Device(..), DeviceType(..))
 import Torch.NN (Linear(..), LinearSpec(..), HasForward(..), sample)
 import qualified Torch.Functional as TF
+import qualified Torch.Functional.Internal as TFI
 import Torch.TensorFactories (zeros')
 import Torch.Tensor ( TensorLike(..)
                     , select , toDouble
@@ -15,7 +16,6 @@ import Torch.Tensor ( TensorLike(..)
                     , (!) , toInt
                     , size , dim
                     , numel, slice)
-import Torch.Functional.Internal (vdot)
 
 {-
  ==================================================================
@@ -23,6 +23,8 @@ import Torch.Functional.Internal (vdot)
  ==================================================================
 
  1. Basic Tensor                           (Line 35  to 89)
+ 1. N Dimensional Tensors                  (Line 35  to 89)
+ 1. Differentiation                        (Line 35  to 89)
  1. Basic autograd example 1               (Line 31  to 47)
  2. Basic autograd example 2               (Line 49  to __)
  3. Loading data from numpy                (Line __  to __)
@@ -35,9 +37,8 @@ import Torch.Functional.Internal (vdot)
                      1. Basic Tensor
  ==================================================================
 -}
-
-tensorsBasic :: IO ()
-tensorsBasic = do
+basicTensors :: IO ()
+basicTensors = do
   let tensor1  = asTensor (1 :: Int)
       tensor2  = asTensor ([1, 2, 3, 4] :: [Int])
       tensor3  = asTensor ([[1, 2, 3, 4], [5,6,7,8]] :: [[Int]])
@@ -93,123 +94,122 @@ tensorsBasic = do
   printTensor "1-mean: " $ TF.mean tensor4
   -- Tensor Float []  2.0000
   printTensor "1-max: " $ TF.max tensor2
+  -- Lots of other related functions can be found
+  -- https://github.com/hasktorch/hasktorch/blob/master/hasktorch/src/Torch/Functional.hs
+  -- https://github.com/hasktorch/hasktorch/blob/master/hasktorch/src/Torch/Functional/Internal.hs
 
 
 {-
  ==================================================================
-                     1. Two Dimensional Tensors
+                     2. N Dimensional Tensors
  ==================================================================
 -}
-
-
-tensorsN :: IO ()
-tensorsN = do
-  let tensor2D = asTensor ([[1, 2, 3, 4], [5,6,7,8]] :: [[Int]])
-      tensor3D = asTensor ([[[0,1,2],[3,4,5]],[[6,7,8],[9,10,11]]] :: [[[Int]]])
-  printTensor "1-select 0 1: " $ select 0 1 tensor2D
+nDimensionalTensors :: IO ()
+nDimensionalTensors = do
+  let tensor2D  = asTensor ([[1, 2, 3, 4], [5, 6, 7, 8]] :: [[Int]])
+      tensor2Da = asTensor ([[1, 2, 3], [1, 2, 3]] :: [[Int]])
+      tensor2Db = asTensor ([[1, 2], [1,2], [1, 2]] :: [[Int]])
+      tensor3D  = asTensor ([[[0,1,2],[3,4,5]],[[6,7,8],[9,10,11]]] :: [[[Int]]])
+  printTensor "2-select 0 1: " $ select 0 1 tensor2D
   -- Tensor Int64 [4] [ 5,  6,  7,  8]
-  printTensor "1-select 1 2: " $ select 1 2 tensor2D
+  printTensor "2-select 1 2: " $ select 1 2 tensor2D
   -- Tensor Int64 [2] [ 3,  7]
-  printTensor "1-size: " $ size 0 tensor2D
-  -- 4
-  printTensor "1-dim: " $ dim tensor2D
-  -- 1
-  printTensor "1-dim: " $ dim tensor3D
+  printTensor "2-size: " $ size 0 tensor2D
+  -- 2
+  printTensor "2-dim: " $ dim tensor2D
+  -- 2
+  printTensor "2-dim: " $ dim tensor3D
   -- 3
-  printTensor "1-shape: " $ shape tensor2D
+  printTensor "2-shape: " $ shape tensor2D
   -- [2, 4]
-  printTensor "1-numel: " $ numel tensor2D
+  printTensor "2-numel: " $ numel tensor2D
   -- 8
-  printTensor "1-view: " $ TF.view [8,1] tensor2D
+  printTensor "2-view: " $ TF.view [8,1] tensor2D
   -- change the shape of the tensor
   -- Tensor Int64 [8,1] [[ 1], [ 2], [ 3], [ 4], [ 5], [ 6], [ 7], [ 8]]
-  printTensor "1-view -1: " $ TF.view [-1,1] tensor2D
+  printTensor "2-view -1: " $ TF.view [-1,1] tensor2D
   -- (-1) will infer the number of rows in the new tensor for us
   -- Tensor Int64 [8,1] [[ 1], [ 2], [ 3], [ 4], [ 5], [ 6], [ 7], [ 8]]
-  printTensor "1-slice 2D: " $ slice 1 1 3 1 tensor2D
+  printTensor "2-slice 2D: " $ slice 1 1 3 1 tensor2D
   -- Tensor Int64 [2,2] [[ 2,  3], [ 6,  7]]
   let r = tensor3D ! (1,0)
   -- (1,0) is ambiguous type variable ExtendedDefaultRules is used
-  printTensor "1-tensor4 ! (1,0): " r
+  printTensor "2-tensor3D ! (1,0): " r
   -- Tensor Int64 [3] [ 6,  7,  8]
-  printTensor "1-add: " $ tensor2D + tensor2D
-  -- Tensor Int64 [4] [ 2,  4,  6,  8]
-  printTensor "1-multiplication: " $ tensor2D * tensor2D
-  -- Tensor Int64 [4] [ 1,  4,  9,  16]
-  printTensor "1-multiplication with scaler: " $ tensor2D * 2
-  -- Tensor Int64 [4] [ 2,  4,  6,  8]
-  -- printTensor "1-dot product: " $ vdot tensor2D tensor2D
-  -- Tensor Int64 []  30
-  -- printTensor "1-mean: " $ TF.mean tensor2D
-  -- Tensor Float []  2.0000
-  -- printTensor "1-max: " $ TF.max tensor2D
-
-
-  -- add 2 tensors
-  -- vector multiplication with scaler 2 * [1,2] == [2,4]
-  -- (Hadamard) product of 2 tensors [2,4] * [2,4] = [4,16]
-  -- dot product [2,4] [2,4] == (2 * 2) + (4 * 4) = 20
-  -- mean [1, -1, 1, -1] == 1/4 (1-1 + 1-1) == 0
-  -- max [1,2,-1,5] == 5
-  -- linespace
-
+  printTensor "2-add: " $ tensor2D + tensor2D
+  -- Tensor Int64 [2,4] [[ 2,  4,  6,  8], [ 10,  12,  14,  16]]
+  printTensor "2-multiplication: " $ tensor2D * tensor2D
+  -- Tensor Int64 [2,4] [[ 1,  4,  9,  16], [ 25,  36,  49,  64]]
+  printTensor "2-multiplication with scaler: " $ tensor2D * 2
+  -- Tensor Int64 [2,4] [[ 2,  4,  6,  8], [ 10,  12,  14,  16]]
+  printTensor "2-dot tensordot: " $ TFI.tensordot tensor2D tensor2D [1] [1]
+  -- Tensor Int64 [2,2] [[ 30,  70], [ 70,  174]]
+  printTensor "2-matrix multiplication: " $ TFI.mm tensor2Da tensor2Db
+  -- Tensor Int64 [2,2] [[ 6,  12], [ 6,  12]]
 
 
 {-
  ==================================================================
-                     1. Basic autograd example 1
+                     3. Differentiation
  ==================================================================
 -}
-
-
-twoBasicAutograd :: IO ()
-twoBasicAutograd = do
+differentiation :: IO ()
+differentiation = do
+  -- Create Tensors
   x <- makeIndependent $ asTensor (1.0 :: Float)
   w <- makeIndependent $ asTensor (2.0 :: Float)
   b <- makeIndependent $ asTensor (3.0 :: Float)
-  let y = toDependent w * toDependent x + toDependent b
+  let dw = toDependent w
+      -- Build a computational graph.
+      y = toDependent w * toDependent x + toDependent b
+      z = dw ^ 2 + 2 * dw + 1
+      -- Compute the gradients
+      gradient = grad z [w]
       gradients = grad y [x, w, b]
-  printTensor "2 - IndependentTensor: " x
+  printTensor "3-IndependentTensor: " x
   -- IndependentTensor {toDependent = Tensor Float []  1.0000   }
-  printTensor "2 - IndependentTensor: " w
-  -- IndependentTensor {toDependent = Tensor Float []  2.0000   }
-  printTensor "2 - IndependentTensor: " b
-  -- IndependentTensor {toDependent = Tensor Float []  3.0000   }
-  printTensor "2 - Gradients: " gradients
+  printTensor "3-Calculation: " z
+  -- Tensor Float []  9.0000
+  printTensor "3-Gradient z [x]: " gradient
+  -- [Tensor Float []  6.0000   ]
+  printTensor "3-Gradients y [x, w, b]: " gradients
   -- [Tensor Float []  2.0000   ,Tensor Float []  1.0000   ,Tensor Float []  1.0000   ]
-  printTensor "2 - Calculation: " y
-  -- Tensor Float []  5.0000
+
 
 {-
  ==================================================================
-                     2. Basic autograd example 2
+                     4. Basic autograd example 2
  ==================================================================
 -}
-
-
-threeBasicAutograd :: IO ()
-threeBasicAutograd = do
+basicAutograd :: IO ()
+basicAutograd = do
   --  Create random tensors of the shape (10, 3) and (10, 2)
   generator <- mkGenerator (Device CPU 0) 0
   let (x, next) = randn' [10, 3] generator
       (y, _)    = randn' [10, 2] next
-  printTensor "3 - Random (10, 3): " x
-  printTensor "3 - Random (10, 2): " y
+  printTensor "4-Random Tensor (10, 3): " x
+  printTensor "4-Random Tensor (10, 2): " y
 
   -- Build a fully connected layer
   linear <- sample $ LinearSpec { in_features = 3, out_features = 2 }
-  printTensor "3 - Weight: " (weight linear)
-  printTensor "3 - Bias: " (bias linear)
+  printTensor "4-Weight: " (toDependent $ weight linear)
+  printTensor "4-Bias: " (toDependent $ bias linear)
 
   -- forward pass
   let prediction = forward linear x
   -- compute loss
       loss = TF.mseLoss prediction y
 
-  printTensor "3 - Prediction: " prediction
-  printTensor "3 - Prediction: " prediction
-  printTensor "3 - Loss: " loss
-  printTensor "3 - Zeros: " (zeros' [2, 3])
+  printTensor "4-Prediction: " prediction
+  printTensor "4-Loss: " loss
+
+  foo <- makeIndependent x
+
+  let gradientWeight = grad (toDependent $ weight linear) [foo]
+      gradientBias = grad (toDependent $ bias linear) [foo]
+
+  printTensor "4-GradientWeight: " gradientWeight
+  printTensor "4-GradientWeight: " gradientBias
 
 -- # Create tensors of shape (10, 3) and (10, 2).
 -- x = torch.randn(10, 3)
@@ -257,7 +257,8 @@ printTensor s t = do
 
 main :: IO ()
 main = do
-  -- tensorsBasic
-  tensorsN
-  --twoBasicAutograd
-  --threeBasicAutograd
+  -- basicTensors
+  -- nDimensionalTensors
+  -- differentiation
+  basicAutograd
+  --basicAutograd
